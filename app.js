73bce0f92f44b2d4d6a7eee815aa7925efc60521
Mistermax12 <90029651+Mistermax12@users.noreply.github.com>
2024-05-06 15:16:44 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,1 @@
-function showMessage() {
-    document.getElementById('messageBox').classList.remove('hidden');
-}
 
-function hideMessage() {
-    document.getElementById('messageBox').classList.add('hidden');
-}
