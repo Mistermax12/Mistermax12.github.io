48cbbcbe7159ef463367c0d6dba762b5ed1d85c2
Mistermax12 <90029651+Mistermax12@users.noreply.github.com>
2024-06-27 17:39:01 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,29 +3,51 @@ const incomePerSecond = incomePerHour / 3600;
 let balance = parseFloat(localStorage.getItem('balance')) || 0;
 let accumulatedCoins = parseFloat(localStorage.getItem('accumulatedCoins')) || 0;
 let lastVisit = parseInt(localStorage.getItem('lastVisit')) || Date.now();
+let telegramUserId = localStorage.getItem('telegramUserId') || '';
 
-// Обновление времени последнего визита
+// Функция для получения и отображения ID пользователя Telegram
+function getTelegramUserId() {
+  fetch('https://api.telegram.org/bot6986401149:AAECAm76PEA0Aa_GoLUVrmjThqEYENa4MMM/getMe')
+    .then(response => response.json())
+    .then(data => {
+      telegramUserId = data.result.id;
+      localStorage.setItem('telegramUserId', telegramUserId);
+      updateTelegramUserIdDisplay();
+    })
+    .catch(error => {
+      console.error('Ошибка при получении данных с Telegram API:', error);
+    });
+}
+
+// Функция для обновления отображаемого ID пользователя Telegram
+function updateTelegramUserIdDisplay() {
+  const telegramUserIdDisplay = document.querySelector('.telegram-user-id');
+  telegramUserIdDisplay.textContent = `Telegram ID: ${telegramUserId}`;
+}
+
+// Функция для обновления времени последнего визита
 function updateLastVisit() {
   lastVisit = Date.now();
   localStorage.setItem('lastVisit', lastVisit);
 }
 
-// Обновление баланса
+// Функция для обновления баланса
 function updateBalance() {
   const now = Date.now();
   const timeElapsed = (now - lastVisit) / 1000;
   accumulatedCoins += timeElapsed * incomePerSecond;
   localStorage.setItem('accumulatedCoins', accumulatedCoins);
   updateLastVisit();
+  updateBalanceDisplay();
 }
 
-// Обновление отображаемого баланса
+// Функция для обновления отображаемого баланса
 function updateBalanceDisplay() {
   const balanceDisplay = document.querySelector('.balance-display');
   balanceDisplay.textContent = `${balance.toFixed(3)} coins`;
 }
 
-// Отображение всплывающей таблички с накопленными монетами
+// Функция для отображения всплывающей таблички с накопленными монетами
 function showClaimPopup() {
   const claimPopup = document.querySelector('.claim-popup');
   const claimMessage = document.querySelector('.claim-message');
@@ -56,8 +78,9 @@ setInterval(() => {
 }, 1000);
 
 // Инициализация
-updateBalance();
 updateBalanceDisplay();
 showClaimPopup();
 updateLastVisit();
+getTelegramUserId(); // Вызываем функцию для получения и отображения ID пользователя Telegram при загрузке страницы
+
 
