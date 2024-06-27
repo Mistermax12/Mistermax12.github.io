c3bb21d7a5d0de74e11c987b26073d6acbc2b72c
Mistermax12 <90029651+Mistermax12@users.noreply.github.com>
2024-06-27 16:36:38 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const incomePerSecond = incomePerHour / 3600;
 let balance = parseFloat(localStorage.getItem('balance')) || 0;
 let accumulatedCoins = parseFloat(localStorage.getItem('accumulatedCoins')) || 0;
 let lastVisit = parseInt(localStorage.getItem('lastVisit')) || Date.now();
-const userId = 12345;  // Замените на реальный user_id
 
 // Обновление времени последнего визита
 function updateLastVisit() {
@@ -34,36 +33,6 @@ function showClaimPopup() {
   claimPopup.classList.add('visible');
 }
 
-// Обновление баланса на сервере
-async function updateBalanceOnServer(userId, balance) {
-  const response = await fetch('http://localhost:5000/update_balance', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      user_id: userId,
-      balance: balance
-    })
-  });
-
-  const data = await response.json();
-  console.log(data);
-}
-
-// Получение баланса с сервера
-async function getBalanceFromServer(userId) {
-  const response = await fetch(`http://localhost:5000/get_balance?user_id=${userId}`);
-  const data = await response.json();
-  if (data.error) {
-    console.error(data.error);
-  } else {
-    balance = data.balance;
-    localStorage.setItem('balance', balance);
-    updateBalanceDisplay();
-  }
-}
-
 // Обработчик кнопки "Claim"
 document.querySelector('.claim-button').addEventListener('click', function() {
   balance += accumulatedCoins;
@@ -72,9 +41,6 @@ document.querySelector('.claim-button').addEventListener('click', function() {
   localStorage.setItem('accumulatedCoins', accumulatedCoins);
   updateBalanceDisplay();
   document.querySelector('.claim-popup').classList.remove('visible');
-
-  // Обновление баланса на сервере
-  updateBalanceOnServer(userId, balance);
 });
 
 // Обработчики для других кнопок
@@ -90,11 +56,8 @@ setInterval(() => {
 }, 1000);
 
 // Инициализация
-getBalanceFromServer(userId).then(() => {
-  updateBalance();
-  updateBalanceDisplay();
-  showClaimPopup();  // Показываем окно claim после получения данных с сервера
-  updateLastVisit();
-});
-
+updateBalance();
+updateBalanceDisplay();
+showClaimPopup();
+updateLastVisit();
 
